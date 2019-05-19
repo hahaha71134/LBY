c6e150019f6abafffb3ef9a9f99c6bb701dffac6
hahaha71134 <37795818+hahaha71134@users.noreply.github.com>
2019-05-19 17:01:27 +0800

    Update Database.py
    When deleting an article in background management,  comments on the article  should also be deleted.

--- project/Database.py
+++ project/Database.py
@@ -5,12 +5,12 @@ import re
 import sqlite3
 import time
 # -*- coding: utf-8 -*-
-
+from flask import  request
 
 
 class Database:
     def browseTime(ip,articled):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
 
 
@@ -21,14 +21,13 @@ class Database:
         r = c.fetchall()
 
         if(r==[]):
-            conn = sqlite3.connect('school2.sqlite3')
+            conn = sqlite3.connect('article.sqlite3')
             c = conn.cursor()
             sql = "Update  article set record=record+1 where articleId=%s" % (articled)
-            print(sql)
             c.execute(sql)
             c.close()
             conn.commit()
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql = "Insert or ignore into browse values('%s',%s)" % (ip, articled)
         c.execute(sql)
@@ -36,19 +35,38 @@ class Database:
         conn.commit()
 
     def selectPersonal(id):
-
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
 
+        ip = request.remote_addr
+        sql2 = "SELECT articleId FROM comment where ip='" + ip + "'"
 
-        sql = "SELECT * FROM article where author='"+id+"'"
+        c.execute(sql2)
+        r2 = c.fetchall()
+        print(r2)
+
+        sql = "SELECT articleId FROM article where author='%s'"%(id)
 
         c.execute(sql)
         r = c.fetchall()
+        r2.extend(r)
+
+        list1 = []  # 鍒涘缓涓€涓柊鐨勬暟缁勬潵瀛樺偍鏃犻噸澶嶅厓绱犵殑鏁扮粍
+        for element in r2:
+            if (element not in list1):
+                list1.append(element)
+        print(list1)
+        list2=[]
+        for articleId in list1:
+            sql3 = "SELECT * FROM article where articleId=%s" % (articleId)
+
+            c.execute(sql3)
+            r3 = c.fetchall()
+            list2.extend(r3)
 
 
         personalInfList=[]
-        for article in r:
+        for article in list2:
             earchRecord = {}
             earchRecord['articleId'] = article[0]
             earchRecord['date'] = article[1]
@@ -82,7 +100,7 @@ class Database:
 
     def selectAll():
 
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
 
 
@@ -97,31 +115,22 @@ class Database:
             earchRecord = {}
             earchRecord['topicName'] = topic[1]
             earchRecord['topicId'] = topic[0]
-            sql2= "SELECT * FROM article where topicId=" + str(topic[0])+" and hidden=1 ORDER BY date DESC"
+            sql2= "SELECT * FROM childTopic where parentTopicId=" + str(topic[0])
 
 
             c.execute(sql2)
             r1 = c.fetchall()
 
+            childTopicList = []
+            for childTopic in r1:
+                childTopicJson = {}
+                childTopicJson['childTopicId']= childTopic[0]
+                childTopicJson['topicName'] =  childTopic[1]
+                childTopicJson['date'] =  childTopic[2]
+                childTopicJson['parentTopicId'] =  childTopic[3]
 
-            articleList = []
-            for article in r1:
-                articleJson = {}
-                articleJson['articleId']=article[0]
-                articleJson['date'] = article[1]
-                articleJson['author'] = article[2]
-                articleJson['goodNum'] = article[3]
-                articleJson['badNum'] = article[4]
-                articleJson['commentNum'] = article[5]
-                articleJson['abstract'] = article[6]
-                articleJson['highlight'] = article[7]
-                articleJson['topicId'] = article[8]
-                articleJson['content'] = article[9]
-                articleJson['articleName'] = article[10]
-                articleJson['record'] = article[11]
-                articleJson['score'] ='%.2f' %((( article[3]-article[4])*0.5+article[11]*0.2+ article[5]*0.3)/(article[3]+article[4]+article[5]+article[11])*100)
-                articleList.append(articleJson)
-            earchRecord['article'] =articleList
+                childTopicList.append(childTopicJson)
+            earchRecord['childTopic'] =childTopicList
             personalInfList.append(earchRecord)
 
 
@@ -132,7 +141,7 @@ class Database:
 
     def selectArticle(id):
 
-            conn = sqlite3.connect('school2.sqlite3')
+            conn = sqlite3.connect('article.sqlite3')
             c = conn.cursor()
 
 
@@ -143,7 +152,7 @@ class Database:
             return r
     def selectArticleContent(id):
 
-            conn = sqlite3.connect('school2.sqlite3')
+            conn = sqlite3.connect('article.sqlite3')
             c = conn.cursor()
 
 
@@ -154,36 +163,46 @@ class Database:
             return r
     def selectComment(id):
 
-            conn = sqlite3.connect('school2.sqlite3')
+            conn = sqlite3.connect('article.sqlite3')
             c = conn.cursor()
 
 
-            sql = "SELECT * FROM comment where articleId="+id+" order by goodNum-badNUm DESC"
-            print(sql)
+            sql = "SELECT * FROM comment where articleId="+id+" order by date DESC"
+
             c.execute(sql)
             r = c.fetchall()
             return r
-    def selectTopic(id):
-        conn = sqlite3.connect('school2.sqlite3')
+    def selectTopic(id,childTopicId):
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
 
-        sql = "SELECT * FROM article where topicId='" + id + "' order by date DESC"
+        if(childTopicId!="0"):
+            sql = "SELECT * FROM article where childTopicId='" + childTopicId + "' and hidden=1 order by date DESC"
+
+
+        else:
+            sql = "SELECT * FROM article where topicId='" + id + "' and hidden=1 order by date DESC"
 
         c.execute(sql)
         r = c.fetchall()
         return r
-    def selectHotArticle(id):
-        conn = sqlite3.connect('school2.sqlite3')
+    def selectHotArticle(id,childTopicId):
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
+        if (childTopicId!="0"):
+            sql = "SELECT * FROM article where childTopicId='" + id + "'" + " and hidden=1  ORDER BY (goodNum*2.5- badNum*2.5 + record + commentNum * 1.5 )/(goodNum*5+badNum*5+record*2+commentNum*3) DESC LIMIT 3"
 
-        sql = "SELECT * FROM article where topicId='" + id + "'"+"ORDER BY goodNum*5-badNum*5+commentNum*3+record*3 LIMIT 3"
+        else:
+            sql = "SELECT * FROM article where topicId='" + id + "'"+" and hidden=1  ORDER BY (goodNum*2.5 - badNum*2.5 + record + commentNum * 1.5 )/(goodNum*5+badNum*5+record*2+commentNum*3) DESC LIMIT 3"
         c.execute(sql)
         r = c.fetchall()
+
+
 
 
         return r
     def selectByKeywords(keywords):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql = "SELECT articleId FROM comment where content like '%" + keywords + "%'"
         c.execute(sql)
@@ -196,14 +215,14 @@ class Database:
 
         r2.extend(r)
 
-        list1 = []  # 创建一个新的数组来存储无重复元素的数组
+        list1 = []  # 鍒涘缓涓€涓柊鐨勬暟缁勬潵瀛樺偍鏃犻噸澶嶅厓绱犵殑鏁扮粍
         for element in r2:
             if (element not in list1):
                 list1.append(element)
         array=[]
-        print(list1)
+
         for article in list1:
-            conn = sqlite3.connect('school2.sqlite3')
+            conn = sqlite3.connect('article.sqlite3')
             c = conn.cursor()
             sql4="SELECT * FROM article where articleId="+ str(article[0])
             c.execute(sql4)
@@ -231,11 +250,11 @@ class Database:
             earchRecord['authorName'] = article[12]
             sql3 = "SELECT * FROM comment where articleId=" + str(article[0])
 
-            conn = sqlite3.connect('school2.sqlite3')
+            conn = sqlite3.connect('article.sqlite3')
             c = conn.cursor()
             c.execute(sql3)
             r3 = c.fetchall()
-            print(r3)
+
             c.close()
             conn.commit()
             commentList = []
@@ -244,8 +263,7 @@ class Database:
                 commentJson['commentId'] = comment[0]
                 commentJson['articleId'] = comment[1]
                 commentJson['content'] = comment[2]
-                print("comment")
-                print(comment[2])
+
                 commentJson['goodNum'] = comment[3]
                 commentJson['badNum'] = comment[4]
 
@@ -253,21 +271,22 @@ class Database:
             earchRecord['comment'] = commentList
             resultInfList.append(earchRecord)
 
-        print(resultInfList)
+
         return resultInfList
 
-    def insertArticle(articleName, authorName, email, articleAbstract, articleHighlight, articleContent, topicId):
-        conn = sqlite3.connect('school2.sqlite3')
+    def insertArticle(articleName, authorName, email, articleAbstract, articleHighlight, articleContent, topicId,childTopicId):
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-        sql = "Insert into article values(NULL,'%s','%s',%s,%s,%s,'%s','%s',%s,'%s','%s',%s,'%s')"%(now_time,email,0,0,0,articleAbstract,articleHighlight,topicId,articleContent,articleName,0,authorName)
-        print(sql)
+
+        sql = "Insert into article values(NULL,'%s','%s',%s,%s,%s,'%s','%s',%s,'%s','%s',%s,'%s',1,'%s')"%(now_time,email,0,0,0,articleAbstract,articleHighlight,topicId,articleContent,articleName,0,authorName,childTopicId)
+
         c.execute(sql)
         c.close()
         conn.commit()
 
     def insertTopic(email, topicName):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
         sql = "Insert into topic values(NULL,'%s','%s','%s')" % (topicName, email, now_time)
@@ -275,16 +294,24 @@ class Database:
 
         c.close()
         conn.commit()
+    def insertChildTopic(parentTopicId, topicName):
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+        sql = "Insert into childTopic values(NULL,'%s','%s',%s)" % (topicName, now_time,parentTopicId )
+        c.execute(sql)
+        c.close()
+        conn.commit()
 
     def goodVote(ip,articleId):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql=" INSERT or  ignore INTO praise VALUES('%s','%s',0,0)"%(ip,articleId)
         c.execute(sql)
         c.close()
         conn.commit()
 
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql1 = "select goodPraise from praise where ip='%s' and article='%s'" % (ip, articleId)
 
@@ -298,7 +325,7 @@ class Database:
             c.execute(sql3)
             c.close()
             conn.commit()
-            print(sql3)
+
         if (r[0]==0):
             sql2="Update praise set goodPraise=1 where ip='%s' and article='%s'"%(ip,articleId)
             c.execute(sql2)
@@ -308,14 +335,14 @@ class Database:
             conn.commit()
 
     def badVote(ip,articleId):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql=" INSERT or  ignore INTO praise VALUES('%s','%s',0,0)"%(ip,articleId)
         c.execute(sql)
         c.close()
         conn.commit()
 
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql1 = "select badPraise from praise where ip='%s' and article='%s'" % (ip, articleId)
 
@@ -339,28 +366,19 @@ class Database:
             conn.commit()
 
     def cGoodVote(ip,commentId):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql=" INSERT or  ignore INTO commentVote VALUES('%s',%s,0,0)"%(ip,commentId)
         c.execute(sql)
         c.close()
         conn.commit()
 
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql1 = "select goodVote from commentVote where ip='%s' and commentId=%s" % (ip, commentId)
 
         c.execute(sql1)
         r = c.fetchone()
-        if (r[0]==1):
-            sql2="Update commentVote set goodVote=0 where ip='%s' and commentId='%s'"%(ip,commentId)
-            c.execute(sql2)
-
-            sql3="Update comment set goodNum=goodNum-1 where commentId=%s"%(commentId)
-            c.execute(sql3)
-            c.close()
-            conn.commit()
-            print(sql3)
         if (r[0]==0):
             sql2="Update commentVote set goodVote=1 where ip='%s' and commentId='%s'"%(ip,commentId)
             c.execute(sql2)
@@ -371,28 +389,20 @@ class Database:
 
 
     def cBadVote(ip,commentId):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql=" INSERT or  ignore INTO commentVote VALUES('%s',%s,0,0)"%(ip,commentId)
         c.execute(sql)
         c.close()
         conn.commit()
-        print(sql)
-        conn = sqlite3.connect('school2.sqlite3')
+
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql1 = "select badVote from commentVote where ip='%s' and commentId=%s" % (ip, commentId)
-        print(sql1)
+
         c.execute(sql1)
         r = c.fetchone()
-        if (r[0]==1):
-            sql2="Update commentVote set badVote=0 where ip='%s' and commentId='%s'"%(ip,commentId)
-            c.execute(sql2)
-            print(sql2)
-            sql3="Update comment set badNum=badNum-1 where commentId=%s"%(commentId)
-            c.execute(sql3)
-            c.close()
-            conn.commit()
-            print(sql3)
+
         if (r[0]==0):
             sql2="Update commentVote set badVote=1 where ip='%s' and commentId='%s'"%(ip,commentId)
             c.execute(sql2)
@@ -403,32 +413,164 @@ class Database:
 
     def selectTopicName():
 
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql = "select topicName from topic"
         c.execute(sql)
         r = c.fetchall()
 
         c.close()
+        conn.commit()
         return r
 
+    def selectAllTopicName():
+
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        sql = "select topicName from topic"
+        c.execute(sql)
+        r = c.fetchall()
+        c.close()
+        conn.commit()
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        sql1 = "select topicName from childTopic"
+        c.execute(sql1)
+        r1 = c.fetchall()
+        c.close()
+        conn.commit()
+        r1.extend(r)
+        return r1
+
     def selectArticleByAuthor(email):
-        conn = sqlite3.connect('school2.sqlite3')
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
         sql = "select date from article where author='%s' order by date DESC LIMIT 1"%(email)
-        print(sql)
+
         c.execute(sql)
         r = c.fetchone()
 
         c.close()
+        conn.commit()
         return r
-
-    def insertCommet(comment,articleId):
-        conn = sqlite3.connect('school2.sqlite3')
+    def selectAllArticleByAuthor(email):
+        conn = sqlite3.connect('article.sqlite3')
         c = conn.cursor()
-        sql = " INSERT into comment VALUES(Null,%s,'%s',0,0)" % (articleId, comment)
+        sql = "select * from article where author='%s' order by date"%(email)
+        c.execute(sql)
+        r = c.fetchall()
+        c.close()
+        conn.commit()
+        return r
+    def selectCommentByIp(ip):
+
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        sql = "select date from comment where ip='%s' order by date DESC LIMIT 1" % (ip)
+
+        c.execute(sql)
+        r = c.fetchone()
+
+        c.close()
+        conn.commit()
+        return r
+    def insertCommet(comment,articleId):
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        ip = request.remote_addr
+        now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+        sql = " INSERT into comment VALUES(Null,%s,'%s',0,0,'%s','%s')" % (articleId, comment,now_time,ip)
+        c.execute(sql)
+        c.close()
+        conn.commit()
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        sql ="Update article set commentNum=commentNum+1 where articleId=%s"%(articleId)
         c.execute(sql)
         c.close()
         conn.commit()
 
+    def selectAllArticle():
 
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        sql = "select * from article"
+        c.execute(sql)
+        r=c.fetchall()
+        c.close()
+        conn.commit()
+        return r
+
+    def hiddenArticle(articleId,ishidden):
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+
+        sql=""
+        if(ishidden=='1'):
+            sql = "Update article set hidden=0 where articleId=%s"%(articleId)
+
+        elif(ishidden=='0'):
+            sql = "Update article set hidden=1 where articleId=%s"%(articleId)
+
+        c.execute(sql)
+        c.close()
+        conn.commit()
+    def deleteArticle(articleId):
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+
+
+        sql = "Delete from article where articleId=%s" % (articleId)
+
+        c.execute(sql)
+        c.close()
+        conn.commit()
+
+    def deleteCommentByArticleId(articleId):
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+
+        sql = "Delete from comment where articleId=%s" % (articleId)
+
+        c.execute(sql)
+        c.close()
+        conn.commit()
+
+    def selectAllComments():
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        sql = "select * from comment"
+        c.execute(sql)
+        r = c.fetchall()
+        c.close()
+        conn.commit()
+        return r
+    def deleteComment(commentId):
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+
+        sql = "Delete from comment where commentId=%s" % (commentId)
+
+        c.execute(sql)
+        c.close()
+        conn.commit()
+    def record(ip):
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+        sql = "Insert or ignore into record values('%s','%s')" % (ip, now_time)
+
+        c.execute(sql)
+        c.close()
+        conn.commit()
+
+    def searchRecord():
+        conn = sqlite3.connect('article.sqlite3')
+        c = conn.cursor()
+        sql="select * from record"
+        c.execute(sql)
+        r=c.fetchall()
+
+        c.close()
+        conn.commit()
+        return r
