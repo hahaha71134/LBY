791f1f7c4a5eb0fc68037c412dc9006892c1e294
hahaha71134 <37795818+hahaha71134@users.noreply.github.com>
2019-06-09 15:23:56 +0800

    Update app.py
    Delete the @app.route ('/management') route, which is a legacy bug that allows users to go directly to the administration page without entering a password.

--- project/app.py
+++ project/app.py
@@ -17,17 +17,15 @@ import difflib
 from Database import Database
 app = Flask(__name__)
 # -*- coding: utf-8 -*-
-#Home page
+
 @app.route('/')
 def mainPage():
     database = Database;
-    ip = request.remote_addr
-    database.record(ip)
+
     mainInf = database.selectAll();
 
     return flask.render_template("mainPage.html", mainInf=mainInf)
 
-#Article content page
 @app.route('/articlePage/<articleId>')
 def articlePage(articleId):
 
@@ -37,80 +35,49 @@ def articlePage(articleId):
         articleInf = database.selectArticle(articleId)
         commentInf=database.selectComment(articleId)
         return flask.render_template("articlePage.html", articleInf=articleInf,commentInf=commentInf)
-@app.route('/toAuthorPageByIp/<ip>')
-def toAuthorPageByIp(ip):
-    database = Database;
-    articleInf = database.selectArticleByIp(ip)
-    return flask.render_template('authorPage.html', articleInf=articleInf);
-
-
-
 
 @app.route('/enterPage')
 def enterPage():
+
+
     return flask.render_template('enterPage.html');
 
-#Topic content page
-@app.route('/topicPage/<topicId>/<childTopicId>')
-def topicPage(topicId,childTopicId):
+@app.route('/topicPage/<topicId>')
+def topicPage(topicId):
     database = Database;
-    articleInf= database.selectTopic(topicId,childTopicId)
-    print(articleInf)
-    articleInfHot=database.selectHotArticle(topicId,childTopicId)
-    articleInfHotList=[]
-    articleInfList=[]
+    articleInf= database.selectTopic(topicId)
+    articleInfHot=database.selectHotArticle(topicId)
+    return flask.render_template('topicPage.html',topicId=topicId, articleInfHot=articleInfHot,articleInf=articleInf);
 
-    for article in articleInfHot:
-        if (article[3] + article[4] + article[5] + article[11]):
-            a = '%.2f' % (50 + (((article[3] - article[4]) * 2.5 + article[11] * 1.5 + article[5]) / (
-                    article[3] * 5 + article[4] * 5 + article[5] * 2 + article[11] * 3) * 100))
-        else:
-            a=0
-        tup2 = (a,)
-        article = article + tup2
-        articleInfHotList.append(article)
-
-    for article in articleInf:
-
-        if (article[3] + article[4] + article[5] + article[11]):
-           a='%.2f' % (50+(((article[3] - article[4]) * 2.5 + article[11]*1.5+ article[5]) / (
-                article[3]*5 + article[4]*5 + article[5]*2 + article[11]*3) * 100))
-        else:
-            a = 0
-        tup2=(a,)
-        article=article+tup2
-        articleInfList.append(article)
-
-    return flask.render_template('topicPage.html',topicId=topicId, childTopicId=childTopicId,articleInfHot=articleInfHotList,articleInf=articleInfList);
-
-#Donate Page
 @app.route('/donatePage')
 def donatePage():
     return flask.render_template('donatePage.html');
-#Search for articles by keyword
+
 @app.route('/selectByKeywords',methods=['POST'])
 def selectResult():
     keywords=request.form['keywords']
     database = Database;
     resultInfList=database.selectByKeywords(keywords)
+
     return flask.render_template('selectResult.html',resultInfList=resultInfList,keywords=keywords);
-#Personal home page
+
 @app.route("/personal/<userId>")
 def select(userId):
     database=Database;
-    personalInf=database.selectPersonal(userId)
-    return flask.render_template("personalPage.html",personalInf=personalInf,email=userId)
 
+    personalInf=database.selectPersonal(userId)
+
+    print(personalInf)
+    return flask.render_template("personalPage.html",personalInf=personalInf,email=userId)
 @app.route('/addArticlePage',methods=['POST'])
 def toAddArticlePage():
     topicId = request.form['topicId']
-    childTopicId=request.form['childTopicId']
-    return flask.render_template('addArticlePage.html',topicId=topicId,childTopicId=childTopicId);
+    return flask.render_template('addArticlePage.html',topicId=topicId);
 
 
 
 
-#Add topic
+
 @app.route('/insertTopic',methods=['POST'])
 def insertTopic():
     email=request.form['email']
@@ -119,6 +86,8 @@ def insertTopic():
     s=''
     database = Database
     allName=database.selectTopicName()
+    print(allName)
+
     for name in allName:
         for c in name[0]:
             if c.isspace():
@@ -133,8 +102,7 @@ def insertTopic():
 
         seq = difflib.SequenceMatcher(None, name[0].lower(), topicName.lower())
         ratio = seq.ratio()
-        print(ratio)
-        if(ratio>0.58):
+        if(ratio>0.43):
             addTopic=0
             break
 
@@ -143,54 +111,16 @@ def insertTopic():
         return redirect(url_for('mainPage'))
     else:
         return "The topic already exists"
-
-#Add subtopics
-@app.route('/insertChildTopic', methods=['POST'])
-def insertChildTopic():
-    parentTopicId = request.form['parentTopicId']
-    topicName = request.form['topicName']
-    addTopic = 1
-    s = ''
-    database = Database
-    allName = database.selectAllTopicName()
-    for name in allName:
-        for c in name[0]:
-            if c.isspace():
-                nameAbbr = name[0].lower().split(" ")
-                s = ''
-                for abbr in nameAbbr:
-                    s += abbr[0:1]
-
-                if (topicName.lower() == s):
-                    addTopic = 0
-                    break
-
-        seq = difflib.SequenceMatcher(None, name[0].lower(), topicName.lower())
-        ratio = seq.ratio()
-        if (ratio >0.9):
-            addTopic = 0
-            break
-
-    if (addTopic):
-        database.insertChildTopic(parentTopicId, topicName)
-        return redirect(url_for('mainPage'))
-    else:
-        return "The topic already exists"
-
 @app.route('/download/<articleId>', methods=['GET'])
 def download_pdf(articleId):
     database = Database
     article=database.selectArticleContent(articleId)
-    name=article[10]
-    name=str(name).encode()
-    print(name)
     with open('templates/test.pdf', 'wb') as f:
         f.write(base64.b64decode(article[9]))
     response = make_response(send_from_directory('templates', 'test.pdf', as_attachment=True))
     response.headers["Content-Disposition"] = (
-        "attachment; filename='{0}'; filename*=UTF-8''{0}".format(article[10].encode('utf-8').decode('ISO-8859-1')+ '.pdf'))
+        "attachment; filename='{0}'; filename*=UTF-8''{0}".format(article[10] + '.pdf'))
     response.headers['Content-Type'] = 'application/pdf'
-
     return response
 
 
@@ -215,6 +145,7 @@ def badVote(articleId):
 @app.route('/cGoodVote/<commentId>',methods=['GET','POST'])
 def cGoodVote(commentId):
     ip = request.remote_addr
+
     database = Database
     database.cGoodVote(ip,commentId)
     return "ok"
@@ -230,7 +161,6 @@ def cBadVote(commentId):
 @app.route('/detection',methods=['POST'])
 def detection():
     topicId = request.form['topicId']
-    childTopicId=request.form['childTopicId']
     articleName = request.form['articleName']
     authorName = request.form['authorName']
     email = request.form['email']
@@ -239,28 +169,27 @@ def detection():
     file = request.files['file']
     encoded = base64.b64encode(file.read())
     file=encoded.decode('ascii')
+
+
     database = Database
+
     now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
-    ip = request.remote_addr
-    r=database.selectArticleByAuthor(email,ip)
+    r=database.selectArticleByAuthor(email)
     if r:
         t=time.mktime(time.strptime(now_time, '%Y-%m-%d %H:%M')) - time.mktime(time.strptime(r[0], '%Y-%m-%d %H:%M'))
         if(t>300):
 
             database.insertArticle(articleName, authorName, email, articleAbstract, articleHighlight, file,
-                               topicId,childTopicId)
-
+                               topicId)
             return "1"
-        else:
-            return "2"
     elif(r==None):
 
         database.insertArticle(articleName, authorName, email, articleAbstract, articleHighlight, file,
-                               topicId,childTopicId)
-
+                               topicId)
         return "1"
-
+    else:
+        return "2"
 
 
 @app.route('/insertComment',methods=['POST'])
@@ -268,78 +197,8 @@ def insertComment():
     articleId = request.form['articleId']
     comment = request.form['comment']
     database = Database
-
-    ip = request.remote_addr
-    now_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
-    r = database.selectCommentByIp(ip)
-    if r:
-        t = time.mktime(time.strptime(now_time, '%Y-%m-%d %H:%M')) - time.mktime(time.strptime(r[0], '%Y-%m-%d %H:%M'))
-        if (t > 300):
-            database.insertCommet(comment, articleId)
-    elif (r == None):
-
-        database.insertCommet(comment, articleId)
+    database.insertCommet(comment,articleId)
     return 'ok'
-
-@app.route('/management')
-def management():
-    database = Database
-    article =database.selectAllArticle()
-
-    return flask.render_template('managementPage.html', article=article);
-
-@app.route('/hidden/<articleId>/<ishidden>')
-def hidden(articleId,ishidden):
-    database = Database
-    database.hiddenArticle(articleId,ishidden)
-
-    article = database.selectAllArticle()
-    return flask.render_template('managementPage.html', article=article);
-
-
-@app.route('/delete/<articleId>')
-def delete(articleId):
-    database = Database
-    database.deleteArticle(articleId)
-    database.deleteComment(articleId)
-    comments = database.selectAllComments()
-    article = database.selectAllArticle()
-    return flask.render_template('managementPage.html', article=article,comments=comments);
-
-
-@app.route('/deleteComment/<commentId>')
-def deleteComment(commentId):
-    database = Database
-    database.deleteComment(commentId)
-
-    comments = database.selectAllComments()
-    article = database.selectAllArticle()
-    return flask.render_template('managementPage.html', article=article,comments=comments);
-@app.route('/toManagementPage')
-def toManagementPage():
-    return flask.render_template('toManagement.html');
-
-
-@app.route('/toAuthorPage/<authorEmail>')
-def toAuthorPage(authorEmail):
-
-    database = Database
-    articleInf=database.selectAllArticleByAuthor(authorEmail)
-    print(articleInf)
-    return flask.render_template('authorPage.html',articleInf=articleInf);
-#To manage the interface
-@app.route('/toManagement',methods=['POST'])
-def toManagement():
-    database = Database
-    psw=request.form['psw']
-    if(psw=="123456"):
-        article = database.selectAllArticle()
-        comments = database.selectAllComments()
-        records=database.searchRecord()
-        return flask.render_template('managementPage.html', article=article,comments=comments,records=records);
-    else:
-        return flask.render_template('toManagement.html');
-
 
 if __name__ == '__main__':
     app.run(debug=True)
